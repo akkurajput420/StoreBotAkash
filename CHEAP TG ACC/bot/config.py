9f0944cf1dd1bac068cc0b0ec40079b69e3ca00b
akkurajput420 <akashrajput7896543@gmail.com>
2026-08-13 17:29:17 +0530

    Update config.py

--- CHEAP TG ACC/bot/config.py
+++ CHEAP TG ACC/bot/config.py
@@ -23,15 +23,15 @@ class Settings(BaseSettings):
     rate_limit_per_minute: int = Field(default=30, alias="RATE_LIMIT_PER_MINUTE")
 
     fampay_api_base_url: str = Field(default="http://payment.openosint.in/api/", alias="FAMPAY_API_BASE_URL")
-    fampay_upi_id: str = Field(default="reflexrohit@fam", alias="FAMPAY_UPI_ID")
+    fampay_upi_id: str = Field(default="raresharmaji@fam", alias="FAMPAY_UPI_ID")
     payment_min_amount: float = Field(default=1.0, alias="PAYMENT_MIN_AMOUNT")
     payment_max_amount: float = Field(default=50000.0, alias="PAYMENT_MAX_AMOUNT")
     payment_poll_interval: int = Field(default=7, alias="PAYMENT_POLL_INTERVAL")
     payment_poll_timeout: int = Field(default=900, alias="PAYMENT_POLL_TIMEOUT")
 
     account_display_name: str = Field(default="TelegramAcc Seller Cheapest", alias="ACCOUNT_DISPLAY_NAME")
-    account_bio: str = Field(default="@TGAccountMarket_Bot", alias="ACCOUNT_BIO")
-    required_channel_link: str = Field(default="https://t.me/+kmak6swDLuZlZjg1", alias="REQUIRED_CHANNEL_LINK")
+    account_bio: str = Field(default="@TgEliteStoreBot", alias="ACCOUNT_BIO")
+    required_channel_link: str = Field(default="https://t.me/Tg_Buyer_Seller", alias="REQUIRED_CHANNEL_LINK")
     spambot_username: str = Field(default="SpamBot", alias="SPAMBOT_USERNAME")
 
     @property
